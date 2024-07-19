f0aa7ab976918279f026a7121fe5abdcc1468d6b
kwoneungbi <rnjsdmsql7290@gmail.com>
2024-07-19 16:16:35 +0900

    fix: ReactDOM.render 호출 업데이트

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./containers/App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <ChakraProvider>
     <App />
-  </ChakraProvider>,
-  document.getElementById("root")
+  </ChakraProvider>
 );
